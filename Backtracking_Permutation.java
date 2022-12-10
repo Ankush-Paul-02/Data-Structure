e2d317731d2e9525f918d652518136c62243c705
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2022-12-11 00:17:16 +0530

    Add files via upload

---
+++ Backtracking_Permutation.java
@@ -0,0 +1,22 @@
+public class Backtracking_Permutation {
+
+    public static void permutations(String str, String ans) {   // O(n*n!)
+        // Base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // Recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            // "abcde" => "ab" + "de" => "abde"
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            permutations(newStr, ans+curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        permutations(str, " ");
+    }
+}
