9dbe7dbfce88de62112cfba12c9dee3e4ecf98f5
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2022-12-10 21:32:34 +0530

    Add files via upload

---
+++ Backtracking_Arrays.java
@@ -0,0 +1,29 @@
+// import java.util.ArrayList;
+
+public class Backtracking_Arrays {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
+    }
+
+    public static void changeArray(int arr[], int index, int val) { // O(n)
+        // base case
+        if(index == arr.length) {
+            printArray(arr);
+            return;
+        }
+        // recursion
+        arr[index] = val;
+        changeArray(arr, index+1, val+1);   // function call step
+        arr[index] = arr[index] - 2;    // backtracking step
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6}; // 3
+        changeArray(arr, 0, 1);
+        printArray(arr);
+    }
+}
