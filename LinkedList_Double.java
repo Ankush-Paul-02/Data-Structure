1f8fc5f6329ba7a8ce3e89152feecdc551479fb7
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2022-12-16 12:31:01 +0530

    Add files via upload

---
+++ LinkedList_Double.java
@@ -0,0 +1,119 @@
+public class LinkedList_Double {
+
+    public class Node {
+        int data;
+        Node next, prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // add First
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // add last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+    // Remove fast
+    public int removeFirst() {
+        if(head == null) {
+            System.out.println("Empty DoublyLinked List");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    // Remove last;
+    public int removeLast() {
+        if(head == null) {
+            System.out.println("Empty DoublyLinked List");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        Node temp = head;
+        while(temp.next.next != null) {
+            temp = temp.next;
+        }
+        int val = temp.next.data;
+        temp.next = null;
+        size--;
+        System.out.println(val + " deleted");
+        return val;
+    }
+
+
+    // print
+    public void print() {
+        Node temp = head;
+        System.out.print("null<-->");
+        while(temp != null) {
+            System.out.print(temp.data + "<-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static void main(String[] args) {
+        LinkedList_Double dll = new LinkedList_Double();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+
+        dll.removeFirst();
+        dll.print();
+
+        dll.addFirst(1);
+        dll.addLast(4);
+        dll.print();
+
+        dll.removeLast();
+        dll.print();
+    }
+}
