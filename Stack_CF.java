3cc596e26ddf5601d8e03cc29f76b8e07679ccf6
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2022-12-18 12:01:29 +0530

    Add files via upload

---
+++ Stack_CF.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Stack_CF {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+    }
+}
