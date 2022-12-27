eecf34c9363b845f7930ff6f7a89d0888ec00e2a
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2022-12-27 21:46:43 +0530

    Add files via upload

---
+++ Array_Unique_Element.java
@@ -0,0 +1,10 @@
+public class Array_Unique_Element {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 1, 6, 3, 6, 2};
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ans = ans^arr[i];
+        }
+        System.out.println(ans);
+    }
+}
