85723c53a0147c515d4edbef1aed5a32709803dd
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2023-02-27 01:23:26 +0530

    Add files via upload

---
+++ Graph_Number_Close_Islands.java
@@ -0,0 +1,64 @@
+public class Graph_Number_Close_Islands {
+
+    static void dfs(int matrix[][], boolean visited[][], int x, int y, int n, int m, boolean hasCornerCell) {   //! O(n*m)
+        if(x < 0 || y < 0 || x >= n || y >= m || visited[x][y] == true || matrix[x][y] == 0) {
+            return;
+        }
+
+        if(x == 0 || y == 0 || x == n-1 || y == m-1) {
+            if(matrix[x][y] == 1) {
+                hasCornerCell = true;
+            }
+        }
+
+        visited[x][y] = true;
+
+        dfs(matrix, visited, x+1, y, n, m, hasCornerCell);
+        dfs(matrix, visited, x, y+1, n, m, hasCornerCell);
+        dfs(matrix, visited, x-1, y, n, m, hasCornerCell);
+        dfs(matrix, visited, x+1, y, n, m, hasCornerCell);
+    }
+
+    static int countClosedIsland(int matrix[][], int n, int m) {
+        boolean visited[][] = new boolean[n][m];
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(i != 0 && j != 0 && i != n-1 && j != m-1 && matrix[i][j] == 1 && visited[i][j] == false) {
+                    boolean hasCornerCell = false;
+                    dfs(matrix, visited, i, j, n, m, hasCornerCell);
+                    if(!hasCornerCell) {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /*
+         *  We have a binary matrix mat[][] of dimensions NXM such that
+         *  1 denotes land and 0 denotes water. Find the number of closed
+         *  islands in the given matrix.
+         *  A closed island is known as the group of 1s that is surrounded
+         *  by only 0s on all the four sides(excluding diagonals). If any 1
+         *  is at the edges of the given matrix then it is not considered as
+         *  the part of the connected island as it is not surrounded by all 0's
+         */
+
+        int N = 5, M = 8;
+
+        int matrix[][] = {
+            {0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 1, 1, 1, 1, 0, 0, 1},
+            {0, 1, 0, 1, 0, 0, 0, 1},
+            {0, 1, 1, 1, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1}
+        };
+
+        System.out.println(countClosedIsland(matrix, N, M));
+    }
+}
---
+++ Graph_Whether_Possible_Finish_Task.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Graph_Whether_Possible_Finish_Task {
+
+    static class Pair {
+        int first, second;
+        Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> make_graph(int numTasks, Vector<Pair> prerequisites) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>(numTasks);
+        
+        for (int i = 0; i < numTasks; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (Pair pair : prerequisites) {
+            graph.get(pair.second).add(pair.first);
+        }
+
+        return graph;
+    }
+
+    static int[] compute_inDegree(ArrayList<ArrayList<Integer>> graph) {
+        int degrees[] = new int[graph.size()];
+
+        for (ArrayList<Integer> neighbors : graph) {
+            for (int neigh : neighbors) {
+                degrees[neigh]++;
+            }
+        }
+        return degrees;
+    }
+
+    static boolean canFinish(int numTasks, Vector<Pair> prerequisites) {    //! O(n)
+        ArrayList<ArrayList<Integer>> graph = make_graph(numTasks, prerequisites);
+        int degrees[] = compute_inDegree(graph);
+
+        for (int i = 0; i < numTasks; i++) {
+            int j = 0;
+            for(; j < numTasks; j++) {
+                if(degrees[j] == 0) {
+                    break;
+                }
+            }
+            if(j == numTasks) {
+                return false;
+            }
+            degrees[j] = -1;
+            for (int neigh : graph.get(j)) {
+                degrees[neigh]--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         *  There are a total of n tasks ypu have to pick, labelled from 
+         *  0 to n-1. Some tasks may have prerequisites, for examples to 
+         *  pick task 0 you have to first pick task 1, which is expressed
+         *  as a pair: [0, 1] Given the total number of tasks and a list
+         *  of prerequisites pairs, is it possible for you to finish all
+         *  tasks.
+         */
+
+        int numTasks = 4;
+        Vector<Pair> prerequisites = new Vector<Pair>();
+
+        prerequisites.add(new Pair(1, 0));
+        prerequisites.add(new Pair(2, 1));
+        prerequisites.add(new Pair(3, 2));
+
+        if(canFinish(numTasks, prerequisites)){
+            System.out.println("Possible to finish all the tasks.");
+        } else {
+            System.out.println("Impossible to finish all the tasks.");
+        }
+    }
+}
