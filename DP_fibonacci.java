adc1b303f12b3835b2e9bc05b6df1e42a7b18ad2
Ankush Paul <119006401+DailyCodeWithAnkush@users.noreply.github.com>
2023-03-03 21:24:15 +0530

    Update DP_fibonacci.java

--- DP_fibonacci.java
+++ DP_fibonacci.java
@@ -1,19 +1,31 @@
 public class DP_fibonacci {
 
-    public static int fibonacci(int n, int dp[]) {
+    public static int fibonacci_memoization(int n, int dp[]) {  //! O(n)
         if(n == 0 || n == 1) {
             return n;
         }
         if(dp[n] != 0) {   //? fib[n] is already calculated
             return dp[n];
         }
-        dp[n] = fibonacci(n-1, dp) + fibonacci(n-2, dp);
+        dp[n] = fibonacci_memoization(n-1, dp) + fibonacci_memoization(n-2, dp);
+        return dp[n];
+    }
+
+    public static int fibonacci_tabulation(int n) { //! O(n)
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 
     public static void main(String[] args) {
         int n = 5;
         int dp[] = new int[n+1];
-        System.out.println(fibonacci(n, dp));
+        System.out.println(fibonacci_memoization(n, dp));
+        System.out.println(fibonacci_tabulation(n));
     }
 }
